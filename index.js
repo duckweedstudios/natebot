b0834ae2bdb3aadc9ca7bf1ceb32161dd52cdab0
coreybriscoe <coreybriscoe@gmail.com>
2022-05-12 15:17:31 -0400

    refactor command interaction

--- index.js
+++ index.js
@@ -32,20 +32,5 @@ for (const file of eventFiles) {
 	}
 }
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
-
 // Login to Discord with your client's token
 client.login(token);

1f130fa2b35127191b53e895924547b1c12d8a54
coreybriscoe <coreybriscoe@gmail.com>
2022-06-13 12:29:54 -0400

    combine setup command into guildjoin

--- commands/guildjoin.js
+++ commands/guildjoin.js
@@ -1,41 +1,93 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const profileModel = require ('../models/profileSchemaGuild');
+const { isMemberOwner } = require('../functions.js');
+const { initializeObject } = require('../functions/serverData');
+const { createHellspeakChannel } = require('../functions/channels.js');
+const { createCondemnedRole } = require('../functions/roles.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('guildjoin')
-		.setDescription('ADMIN ONLY'),
+		.setDescription('[owner] Setup the Natebot on the server as desired')
+		.addUserOption(userOption => userOption
+			.setName('first-condemned').setDescription('Optionally specify the first Condemned Soul user, otherwise it will be you...'))
+		.addIntegerOption(intOption => intOption
+			.setName('mean-delay').setDescription('The mean delay between hauntings in minutes. Defaults to 1440 (24 hours).'))
+		.addIntegerOption(intOption => intOption
+			.setName('randomness').setDescription('The randomness metric for hauntings. Higher gives more variation. Defaults to 5.'))
+		.addRoleOption(roleOption => roleOption
+			.setName('mod-role').setDescription('The moderator role which can interact with the bot\'s settings.')),
 	async execute(interaction) {
+		// Check for admin status
+		if (isMemberOwner(interaction.member, interaction.client, interaction.guild)) {
+			interaction.reply({ content: 'You must be an admin to use this command!', ephemeral: true });
+			return;
+		}
+		// Check if arguments are valid
+		let meanDelay = interaction.options.getInteger('mean-delay');
+		if (!meanDelay) {
+			meanDelay = 1440;
+		} else if (meanDelay < 2) {
+			interaction.reply({ content: 'The mean delay must be at least two minutes.', ephemeral: true });
+			return;
+		}
+		let randomness = interaction.options.getInteger('randomness');
+		if (!randomness) {
+			randomness = 5;
+		} else if (randomness < 1 || randomness > 10) {
+			interaction.reply({ content: 'Randomness must be an integer between 1 and 10.', ephemeral: true });
+			return;
+		}
+		// Ensure target isn't a bot
+		if (interaction.options.getMember('first-condemned') && interaction.options.getMember('first-condemned').user.bot) {
+			if (interaction.options.getMember('first-condemned').id === '974345779349184542') {
+				interaction.reply({ content: 'I am flattered, but I must refuse. Choose a user instead.', ephemeral: true });
+				return;
+			} else {
+				interaction.reply({ content: 'A puny bot cannot bear the mantle of Condemned Soul. Choose a user instead.', ephemeral: true });
+				return;
+			}
+		}
+
+		// Create the condemned soul role on the server (assuming it doesn't exist)
+		let condemnedRole;
 		try {
-			const profile = await profileModel.create({
-				serverId: interaction.guild.id,
-				condemnedMember: interaction.user.id,
-				newSoulMade: false,
-				settings: {
-					paused: false,
-					condemnedRoleId: '984925367070523423',
-					channelId: '981563688215523438',
-					modRoles: '974364005789487114',
-				},
-				schedule: {
-					next: {
-						time: null,
-						soulTypeId : null,
-					},
-					past: {
-						time: null,
-						soulTypeId : null,
-					},
-					meanDelay: 1440,
-					variaton: 5,
-				},
-				stats: {
-					serverSoulsCaught: 0,
-					hauntingsCount: 0,
-					soulsCreated: 0,
-					lastCondemnedMember: 0,
-				},
-			});
+			condemnedRole = await createCondemnedRole(interaction.guild);
+		} catch (err) {
+			console.error(err);
+			interaction.reply({ content: 'Setup failed (could not create role), please try again later.', ephemeral: true });
+			return;
+		}
+		
+		// Assign first condemned (save user id) and assign the role
+		let memberTarget;
+		if (!interaction.options.getMember('first-condemned')) {
+			memberTarget = interaction.member.id;
+			interaction.member.roles.add((await condemnedRole));
+		} else {
+			memberTarget = interaction.options.getMember('first-condemned').id;
+			interaction.options.getMember('first-condemned').roles.add((await condemnedRole));
+		}
+
+		// Create the HELLSPEAK voice channel
+		let hellspeakChannel;
+		try {
+			hellspeakChannel = createHellspeakChannel(interaction.guild, condemnedRole);
+		} catch (err) {
+			console.error(`Error in setup.js: Could not create HELLSPEAK channel: ${err}`);
+			interaction.reply({ content: `Setup failed (could not create a voice channel), please try again later.`, ephemeral: true });
+			return;
+		}
+
+		try {
+			const profile = await profileModel.create(
+				initializeObject(interaction.guild.id,
+					memberTarget,
+					(await condemnedRole.id),
+					hellspeakChannel.id,
+					(interaction.options.getRole('mod-role') ? interaction.options.getRole('mod-role') : ''),
+					meanDelay,
+					randomness));
 			profile.save();
 			await interaction.reply({ content: 'setup success\nFETCH ME THEIR SOULS!', ephemeral: true });
 		} catch (error) {

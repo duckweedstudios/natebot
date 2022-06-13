1dd401c26bf02490eb31b958a0e7d302bb73b6c9
Zade Dohan <zadohan@hotmail.com>
2022-06-13 19:58:55 -0400

    Mute Button Functional

--- buttons/muteButton.js
+++ buttons/muteButton.js
@@ -1,6 +1,5 @@
 const { MessageButton } = require('discord.js');
 const { getTarget } = require('../events/getTarget');
-const { getMutedRoleOnServer } = require('../functions/roles');
 const { increaseValue } = require('../events/inc');
 const { getSoulData } = require('../events/query');
 
@@ -13,18 +12,21 @@ module.exports = {
         
 	async execute(interaction) {
 		const target = getTarget(interaction);
-		const mutedRole = await getMutedRoleOnServer(interaction.guild);
 		const member = interaction.guild.members.cache.get(target.id);
 		const soulData = getSoulData(interaction, interaction.user.id);
 		if (soulData.souls <= 1) {
 			await interaction.reply({ content: 'You do not have enough souls left', ephemeral: true });
 		} else {
 			increaseValue(interaction, interaction.user, 'souls', -1);
-			member.roles.add(mutedRole);
-			setTimeout(() => {
-				member.roles.remove(mutedRole);
-			}, 150000);
-			await interaction.reply({ content:`You spent 1 soul to mute ${target.username} for 150 seconds`, ephemeral: true });
+			if (member.voice.serverMute) {
+				await interaction.reply({ content: 'This user is already Muted', ephemeral: true });
+			} else {
+				member.voice.setMute(true);
+				setTimeout(() => {
+					member.voice.setMute(false);
+				}, 150000);
+				await interaction.reply({ content:`You spent 1 soul to mute ${target.username} for 150 seconds`, ephemeral: true });
+			}
 		}
 	},
 };

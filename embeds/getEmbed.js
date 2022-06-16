b43a867a0f00d579e163a7e3392e33c275a78992
coreybriscoe <coreybriscoe@gmail.com>
2022-06-16 01:05:02 -0400

    reorganize getEmbed for (hopefully) efficiency

--- embeds/getEmbed.js
+++ embeds/getEmbed.js
@@ -37,60 +37,6 @@ module.exports = {
 		if (guildData.condemnedMember === target.id) { targetIsCondemned = true; }
 		if (guildData.condemnedMember === interaction.user.id) { userIsCondemned = true; }
 	
-		// Defining Embeds
-		// Error Embed
-		const errorEmbed = new MessageEmbed()
-			.setColor('BLUE')
-			.setTitle(`__***EMBED ERROR***__`)
-			.setAuthor({ name: target.tag, iconURL: target.displayAvatarURL({ dynamic: true }) })
-			.setDescription('*There was an error when producing this embed*')
-			.setThumbnail('https://i.imgur.com/T9HDICa.jpeg')
-			.setTimestamp();
-
-		// Condemned Self Embed
-		const condemnedSelfEmbed = new MessageEmbed()
-			.setColor('DARK_RED')
-			.setTitle(`__**Your Profile**__`)
-			.setAuthor({ name: target.tag, iconURL: target.displayAvatarURL({ dynamic: true }) })
-			.setDescription('*THE CONDEMNED SOUL*')
-			.setThumbnail('https://imgur.com/MXLHd9R.png')
-			.addFields(
-				{ name: '\u200B', value: '\u200B' },
-				{ name: '__Your Souls Left__', value: `*${soulData.souls}*` },
-				{ name: '__Spent Souls__', value: `*${(100 - soulData.soulsCaught - soulData.souls)}*`, inline: true },
-				{ name: '__Souls Stolen__', value: `*${soulData.soulsCaught}*`, inline: true },
-				{ name: '\u200B', value: '\u200B' },
-				{ name: '__**Condemned Career Stats**__', value: `\u200B` },
-				{ name: '__Times as Condemned__', value: `*${soulData.condemnedCount}*` },
-				{ name: '__Fetcher Rank__', value: `*${soulData.soulXP}*`, inline: true },
-				{ name: '__Fooled Count__', value: `*${soulData.fooledCount}*`, inline: true },
-			)
-			.setTimestamp()
-			.setFooter({ text: 'Powered by Parkie LLC' });
-
-		// Condemned Other Embed
-		const condemnedOtherEmbed = new MessageEmbed()
-			.setColor('DARK_ORANGE')
-			.setTitle(`__**${target.username}'s profile**__`)
-			.setAuthor({ name: target.tag, iconURL: target.displayAvatarURL({ dynamic: true }) })
-			.setDescription('*//Professional Soul Fetcher//*')
-			.setThumbnail('https://i.imgur.com/rgbM2hX.jpg')
-			.addFields(
-				{ name: 'XP:', value: `${soulData.soulXP}` },
-				{ name: '\u200B', value: '\u200B' },
-				{ name: '__Current Souls:__', value: `*${soulData.souls}*` },
-				{ name: '__Souls Caught:__', value: `*${soulData.soulsCaught}*`, inline: true },
-				{ name: '__Fetch Count:__', value: `*${soulData.fetchCount}*`, inline: true },
-				{ name: '\u200B', value: '\u200B' },
-				{ name: '__***       Career Stats       ***__', value: '\u200B' },
-				{ name: '__Times as Condemned:__', value: `*${soulData.condemnedCount}*`, inline: true },
-				{ name: '__Was Fooled Count:__', value: `*${soulData.gotFooledCount}*`, inline: true },
-				{ name: '__Rank:__', value: `*${soulData.soulXP}*` },
-			)
-			.setTimestamp()
-			.setFooter({ text: 'Powered by Parkie LLC' });
-	
-		// Fetcher Self Embed
 		let soulTierData;
 		try {
 			soulTierData = getSoulTier(soulData.soulXP);
@@ -98,85 +44,126 @@ module.exports = {
 			console.error('Error in getEmbed.js: Could not get soul tier data (eg fetcher tier): ' + err);
 			return;
 		}
-		const fetcherSelfEmbed = new MessageEmbed()
-			.setColor('RED')
-			.setTitle(`__**Your Profile**__`)
-			.setAuthor({ name: target.tag, iconURL: target.displayAvatarURL({ dynamic: true }) })
-			.setDescription(`*${soulTierData.tierName} Soul Fetcher*`)
-			.setThumbnail('https://i.imgur.com/rgbM2hX.jpg')
-			.addFields(
-				{ name: 'Rank:', value: `**${soulTierData.tierNum}** (${soulData.soulXP} XP)` },
-				// { name: '\u200B', value: '\u200B' },
-				{ name: '__Your Current Souls:__', value: `*${soulData.souls}*` },
-				{ name: '__Souls Caught:__', value: `*${soulData.soulsCaught}*`, inline: true },
-				{ name: '__Fetch Count:__', value: `*${soulData.fetchCount}*`, inline: true },
-				{ name: '\u200B', value: '\u200B' },
-				{ name: '__***       Career Stats       ***__', value: '\u200B' }, // don't know why just putting a space breaks this
-				{ name: '__Times as Condemned:__', value: `*${soulData.condemnedCount}*`, inline: true },
-				{ name: '__Was Fooled Count:__', value: `*${soulData.gotFooledCount}*`, inline: true },
-			)
-			.setTimestamp()
-			.setFooter({ text: 'Powered by Parkie LLC' });
-		
-		// Fetcher Other Embed
-		const fetcherOtherEmbed = new MessageEmbed()
-			.setColor('DARK_ORANGE')
-			.setTitle(`__**The profile of ${target.username}**__`)
-			.setAuthor({ name: target.tag, iconURL: target.displayAvatarURL({ dynamic: true }) })
-			.setDescription(`*${soulTierData.tierName} Soul Fetcher*`)
-			.setThumbnail('https://i.imgur.com/rgbM2hX.jpg')
-			.addFields(
-				{ name: 'Rank:', value: `**${soulTierData.tierNum}** (${soulData.soulXP} XP)` },
-				// { name: '\u200B', value: '\u200B' },
-				{ name: '__Current Souls:__', value: `*${soulData.souls}*` },
-				{ name: '__Souls Caught:__', value: `*${soulData.soulsCaught}*`, inline: true },
-				{ name: '__Fetch Count:__', value: `*${soulData.fetchCount}*`, inline: true },
-				{ name: '\u200B', value: '\u200B' },
-				{ name: '__***       Career Stats       ***__', value: '\u200B' },
-				{ name: '__Times as Condemned:__', value: `*${soulData.condemnedCount}*`, inline: true },
-				{ name: '__Was Fooled Count:__', value: `*${soulData.gotFooledCount}*`, inline: true },
-			)
-			.setTimestamp()
-			.setFooter({ text: 'Powered by Parkie LLC' });
-		
-		// Fetcher Looking At Condemned Embed
-		const fetcherCondemnedEmbed = new MessageEmbed()
-			.setColor('DARK_RED')
-			.setTitle(`__**${target.username}**__`)
-			.setAuthor({ name: target.tag, iconURL: target.displayAvatarURL({ dynamic: true }) })
-			.setDescription('*THE CONDEMNED SOUL*')
-			.setThumbnail('https://imgur.com/MXLHd9R.png')
-			.addFields(
-				{ name: '\u200B', value: '\u200B' },
-				{ name: '__Souls Left__', value: `*${soulData.souls}*` },
-				{ name: '__Souls Stolen__', value: `*${soulData.soulsCaught}*` },
-				{ name: '\u200B', value: '\u200B' },
-				{ name: '__**Condemned Career Stats**__', value: `\u200B` },
-				{ name: '__Times as Condemned__', value: `*${soulData.condemnedCount}*` },
-				{ name: '__Fetcher Rank__', value: `**${soulTierData.tierNum}** (${soulData.soulXP} XP)`, inline: true },
-				{ name: '__Fooled Count__', value: `*${soulData.fooledCount}*`, inline: true },
-			)
-			.setTimestamp()
-			.setFooter({ text: 'Powered by Parkie LLC' });
 		
 		// Outputting the Embed
 		try {
 			if (self) {
 				if (userIsCondemned) {
-					return condemnedSelfEmbed;
+					// Condemned Self Embed
+					return new MessageEmbed()
+						.setColor('DARK_RED')
+						.setTitle(`__**Your Profile**__`)
+						.setAuthor({ name: target.tag, iconURL: target.displayAvatarURL({ dynamic: true }) })
+						.setDescription('*THE CONDEMNED SOUL*')
+						.setThumbnail('https://imgur.com/MXLHd9R.png')
+						.addFields(
+							{ name: '\u200B', value: '\u200B' },
+							{ name: '__Your Souls Left__', value: `*${soulData.souls}*` },
+							{ name: '__Spent Souls__', value: `*${(100 - soulData.soulsCaught - soulData.souls)}*`, inline: true },
+							{ name: '__Souls Stolen__', value: `*${soulData.soulsCaught}*`, inline: true },
+							{ name: '\u200B', value: '\u200B' },
+							{ name: '__**Condemned Career Stats**__', value: `\u200B` },
+							{ name: '__Times as Condemned__', value: `*${soulData.condemnedCount}*` },
+							{ name: '__Fetcher Rank__', value: `*${soulData.soulXP}*`, inline: true },
+							{ name: '__Fooled Count__', value: `*${soulData.fooledCount}*`, inline: true },
+						)
+						.setTimestamp()
+						.setFooter({ text: 'Powered by Parkie LLC' });
 				} else {
-					return fetcherSelfEmbed;
+					// Fetcher Self Embed
+					return new MessageEmbed()
+						.setColor('RED')
+						.setTitle(`__**Your Profile**__`)
+						.setAuthor({ name: target.tag, iconURL: target.displayAvatarURL({ dynamic: true }) })
+						.setDescription(`*${soulTierData.tierName} Soul Fetcher*`)
+						.setThumbnail('https://i.imgur.com/rgbM2hX.jpg')
+						.addFields(
+							{ name: 'Rank:', value: `**${soulTierData.tierNum}** (${soulData.soulXP} XP)` },
+							// { name: '\u200B', value: '\u200B' },
+							{ name: '__Your Current Souls:__', value: `*${soulData.souls}*` },
+							{ name: '__Souls Caught:__', value: `*${soulData.soulsCaught}*`, inline: true },
+							{ name: '__Fetch Count:__', value: `*${soulData.fetchCount}*`, inline: true },
+							{ name: '\u200B', value: '\u200B' },
+							{ name: '__***       Career Stats       ***__', value: '\u200B' }, // don't know why just putting a space breaks this
+							{ name: '__Times as Condemned:__', value: `*${soulData.condemnedCount}*`, inline: true },
+							{ name: '__Was Fooled Count:__', value: `*${soulData.gotFooledCount}*`, inline: true },
+						)
+						.setTimestamp()
+						.setFooter({ text: 'Powered by Parkie LLC' });
 				}
 			} else if (userIsCondemned) {
-				return condemnedOtherEmbed;
+				// Condemned Other Embed
+				return new MessageEmbed()
+					.setColor('DARK_ORANGE')
+					.setTitle(`__**${target.username}'s profile**__`)
+					.setAuthor({ name: target.tag, iconURL: target.displayAvatarURL({ dynamic: true }) })
+					.setDescription(`*${soulTierData.tierName} Soul Fetcher*`)
+					.setThumbnail('https://i.imgur.com/rgbM2hX.jpg')
+					.addFields(
+						{ name: 'Rank:', value: `**${soulTierData.tierNum}** (${soulData.soulXP} XP)` },
+						{ name: '\u200B', value: '\u200B' },
+						{ name: '__Current Souls:__', value: `*${soulData.souls}*` },
+						{ name: '__Souls Caught:__', value: `*${soulData.soulsCaught}*`, inline: true },
+						{ name: '__Fetch Count:__', value: `*${soulData.fetchCount}*`, inline: true },
+						{ name: '\u200B', value: '\u200B' },
+						{ name: '__***       Career Stats       ***__', value: '\u200B' },
+						{ name: '__Times as Condemned:__', value: `*${soulData.condemnedCount}*`, inline: true },
+						{ name: '__Was Fooled Count:__', value: `*${soulData.gotFooledCount}*`, inline: true },
+					)
+					.setTimestamp()
+					.setFooter({ text: 'Powered by Parkie LLC' });
 			} else if (targetIsCondemned) {
-				return fetcherCondemnedEmbed;
+				// Fetcher Looking At Condemned Embed
+				return new MessageEmbed()
+					.setColor('DARK_RED')
+					.setTitle(`__**${target.username}**__`)
+					.setAuthor({ name: target.tag, iconURL: target.displayAvatarURL({ dynamic: true }) })
+					.setDescription('*THE CONDEMNED SOUL*')
+					.setThumbnail('https://imgur.com/MXLHd9R.png')
+					.addFields(
+						{ name: '\u200B', value: '\u200B' },
+						{ name: '__Souls Left__', value: `*${soulData.souls}*` },
+						{ name: '__Souls Stolen__', value: `*${soulData.soulsCaught}*` },
+						{ name: '\u200B', value: '\u200B' },
+						{ name: '__**Condemned Career Stats**__', value: `\u200B` },
+						{ name: '__Times as Condemned__', value: `*${soulData.condemnedCount}*` },
+						{ name: '__Fetcher Rank__', value: `**${soulTierData.tierNum}** (${soulData.soulXP} XP)`, inline: true },
+						{ name: '__Fooled Count__', value: `*${soulData.fooledCount}*`, inline: true },
+					)
+					.setTimestamp()
+					.setFooter({ text: 'Powered by Parkie LLC' });
 			} else {
-				return fetcherOtherEmbed;
+				// Fetcher Other Embed
+				return new MessageEmbed()
+					.setColor('DARK_ORANGE')
+					.setTitle(`__**The profile of ${target.username}**__`)
+					.setAuthor({ name: target.tag, iconURL: target.displayAvatarURL({ dynamic: true }) })
+					.setDescription(`*${soulTierData.tierName} Soul Fetcher*`)
+					.setThumbnail('https://i.imgur.com/rgbM2hX.jpg')
+					.addFields(
+						{ name: 'Rank:', value: `**${soulTierData.tierNum}** (${soulData.soulXP} XP)` },
+						// { name: '\u200B', value: '\u200B' },
+						{ name: '__Current Souls:__', value: `*${soulData.souls}*` },
+						{ name: '__Souls Caught:__', value: `*${soulData.soulsCaught}*`, inline: true },
+						{ name: '__Fetch Count:__', value: `*${soulData.fetchCount}*`, inline: true },
+						{ name: '\u200B', value: '\u200B' },
+						{ name: '__***       Career Stats       ***__', value: '\u200B' },
+						{ name: '__Times as Condemned:__', value: `*${soulData.condemnedCount}*`, inline: true },
+						{ name: '__Was Fooled Count:__', value: `*${soulData.gotFooledCount}*`, inline: true },
+					)
+					.setTimestamp()
+					.setFooter({ text: 'Powered by Parkie LLC' });
 			}
 		} catch (error) {
 			console.log(error);
-			return errorEmbed;
+			// Error Embed
+			return new MessageEmbed()
+				.setColor('BLUE')
+				.setTitle(`__***EMBED ERROR***__`)
+				.setAuthor({ name: target.tag, iconURL: target.displayAvatarURL({ dynamic: true }) })
+				.setDescription('*There was an error when producing this embed*')
+				.setThumbnail('https://i.imgur.com/T9HDICa.jpeg')
+				.setTimestamp();
 		}
 	},
 };

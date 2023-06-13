ef51dfc5897fada817e07ebf8084c2e3bdc02018
Zade Dohan <105446965+zade0123@users.noreply.github.com>
2023-06-13 16:51:09 -0400

    inc.js refactor

--- events/inc.js
+++ events/inc.js
@@ -6,7 +6,6 @@ module.exports = {
 	increaseValue : async (interaction, target, datapoint, value) => {
 		try {
 			const targetData = await profileModel.findOne({ fetcherID: target.id });
-			if (!targetData) return editPrompt(interaction, ({ content: `There is no user with the username ${target.username}`, ephemeral: true }));
 			await profileModel.findOneAndUpdate({ fetcherID: target.id }, {
 				$inc: {
 					[datapoint]: value,
@@ -14,7 +13,6 @@ module.exports = {
 			});
 		} catch (err) {
 			console.log(err);
-			editPrompt(interaction, ({ content: `There was an error`, components: [], ephemeral: true }));
 		}
 	},
 };

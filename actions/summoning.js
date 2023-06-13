ee32c76fb4af5b0c23eaa66f5b716b9aa6922b7c
coreybriscoe <coreybriscoe@gmail.com>
2023-06-13 15:58:27 -0400

    Prevent summoning too soon after real haunting

--- actions/summoning.js
+++ actions/summoning.js
@@ -1,20 +1,45 @@
 const dayjs = require('dayjs');
 const { hauntSomeChannelWithSoul } = require('./hauntings');
 const { getWeightedRandomSoulType } = require('../functions/souls');
+const { getGuildData } = require('../events/guildquery');
 dayjs.extend(require('dayjs/plugin/duration'));
 dayjs.extend(require('dayjs/plugin/relativeTime'));
 
 const summoningCooldown = dayjs.duration(1, 'minute');
+const delayAfterRealHaunting = dayjs.duration(1, 'minute');
 
 module.exports = {
+	SummonAttemptResults: {
+		Success: 0,
+		Cooldown: 1,
+		TooSoonAfterRealHaunting: 2,
+	},
 	attemptSummoning: async (client, guild) => {
 		const currentTimestamp = dayjs();
-		if (!client.nateBotData[guild.id].lastSummonTime || currentTimestamp.diff(client.nateBotData[guild.id].lastSummonTime, 'minute') >= summoningCooldown.asMinutes()) {
+		const guildData = await getGuildData(guild.id);
+		if (
+			currentTimestamp.diff(guildData.schedule.past.time, 'minutes') <= delayAfterRealHaunting.asMinutes()
+			&& currentTimestamp.diff(guildData.schedule.past.time, 'minutes') >= 0
+		) {
+			return {
+				summonSuccess: module.exports.SummonAttemptResults.TooSoonAfterRealHaunting,
+				cooldown: delayAfterRealHaunting.subtract(currentTimestamp.diff(guildData.schedule.past.time, 'minutes'), 'minute').humanize(),
+			};
+		} else if (
+			!client.nateBotData[guild.id].lastSummonTime
+			|| currentTimestamp.diff(client.nateBotData[guild.id].lastSummonTime, 'minute') >= summoningCooldown.asMinutes()
+		) {
 			hauntSomeChannelWithSoul(guild, getWeightedRandomSoulType(guild.id));
 			client.nateBotData[guild.id].lastSummonTime = currentTimestamp;
-			return { summonSuccess: true, cooldown: summoningCooldown.humanize() };
+			return {
+				summonSuccess: module.exports.SummonAttemptResults.Success,
+				cooldown: summoningCooldown.humanize(),
+			};
 		} else {
-			return { summonSuccess: false, cooldown: summoningCooldown.subtract(currentTimestamp.diff(client.nateBotData[guild.id].lastSummonTime, 'minute'), 'minute').humanize() };
+			return {
+				summonSuccess: module.exports.SummonAttemptResults.Cooldown,
+				cooldown: summoningCooldown.subtract(currentTimestamp.diff(client.nateBotData[guild.id].lastSummonTime, 'minute'), 'minute').humanize(),
+			};
 		}
 	},
 };
--- buttons/summonButton.js
+++ buttons/summonButton.js
@@ -1,5 +1,6 @@
 const { MessageButton } = require('discord.js');
 const { attemptSummoning } = require('../actions/summoning.js');
+const { SummonAttemptResults } = require('../actions/summoning.js');
 
 module.exports = {
 	name: 'summonButton',
@@ -10,10 +11,12 @@ module.exports = {
         
 	async execute(interaction) {
 		const { summonSuccess, cooldown } = await attemptSummoning(interaction.client, interaction.member.guild);
-		if (summonSuccess) {
+		if (summonSuccess === SummonAttemptResults.Success) {
 			await interaction.reply({ content:`A soul was summoned! You can summon again in ${cooldown}.`, ephemeral: true });
-		} else {
+		} else if (summonSuccess === SummonAttemptResults.Cooldown) {
 			await interaction.reply({ content:`You cannot summon a soul for ${cooldown}.`, ephemeral: true });
+		} else if (summonSuccess === SummonAttemptResults.TooSoonAfterRealHaunting) {
+			await interaction.reply({ content:`You cannot summon a soul for ${cooldown} because a real haunting has occurred recently.`, ephemeral: true });
 		}
 	},
 };

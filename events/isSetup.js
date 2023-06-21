c24e98cea257f44b8fbba19c498b3c2dd191ae68
Zade Dohan <105446965+zade0123@users.noreply.github.com>
2023-06-21 11:45:02 -0400

    created isSetup functions

---
+++ events/isSetup.js
@@ -0,0 +1,22 @@
+const { getSoulData } = require('../events/query');
+const { getGuildData } = require('../events/guildquery');
+
+module.exports = {
+	isUserSetup : async (interaction, userId) => {
+        const soulData = await getSoulData(interaction, userId)
+        if (!soulData) {
+            return false;
+        } else {
+            return true;
+        };
+	},
+
+    isGuildSetup : async (interaction) => {
+        const guildData = await getGuildData(interaction)
+        if (!guildData) {
+            return false;
+        } else {
+            return true;
+        };
+	}
+};

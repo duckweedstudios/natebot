40953650bfa24f2046c2cd09d4c304584659cc74
coreybriscoe <coreybriscoe@gmail.com>
2023-06-21 18:27:56 -0400

    fix: Various fixes
    Changed slash command options to use correct types instead of just userOption
    
    Changed the error message in setServerSetting for clarity
    
    Remove use of 'settings.' prefix to certain settings
    
    Add notice that currently scheduled hauntings are unaffected by changing settings

--- commands/soulsadmin.js
+++ commands/soulsadmin.js
@@ -7,11 +7,11 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('soulsadmin')
 		.setDescription('Admin options for the bot. Leave all options blank to see current settings.')
-		.addUserOption(userOption => userOption
+		.addBooleanOption(booleanOption => booleanOption
 			.setName('reset-defaults').setDescription('Reset the server to default settings').setRequired(false))
-		.addUserOption(userOption => userOption
+		.addIntegerOption(integerOption => integerOption
 			.setName('mean-delay').setDescription('Set the mean delay between haunts in minutes (default 1440 = 1 day)').setRequired(false))
-		.addUserOption(userOption => userOption
+		.addIntegerOption(integerOption => integerOption
 			.setName('variation').setDescription('Set the variation of the delay between haunts from 1 to 10 (default 6)').setRequired(false)),
 	async execute(interaction) {
 		if (!await isGuildSetup(interaction)) {
@@ -35,7 +35,7 @@ module.exports = {
 						if (interaction.options.getInteger('mean-delay') < 2) {
 							response += 'Mean delay must be at least 2 minutes.\n';
 						} else {
-							response += `Mean delay updated to ${interaction.options.getInteger('mean-delay')} minutes.\n}`;
+							response += `Mean delay updated to ${interaction.options.getInteger('mean-delay')} minutes.\n`;
 							setServerSetting(interaction.guild.id, 'meanDelay', interaction.options.getInteger('mean-delay'));
 						}
 					}
@@ -43,7 +43,7 @@ module.exports = {
 						if (interaction.options.getInteger('variation') < 1 || interaction.options.getInteger('variation') > 10) {
 							response += 'Variation must be between 1 and 10.\n';
 						} else {
-							response += `Variation updated to ${interaction.options.getInteger('variation')}.\n}`;
+							response += `Variation updated to ${interaction.options.getInteger('variation')}.\n`;
 							setServerSetting(interaction.guild.id, 'variation', interaction.options.getInteger('variation'));
 						}
 					}
@@ -56,6 +56,8 @@ module.exports = {
 			if (response === '') {
 				const currentServerSettings = await getServerSettings(interaction.guild.id);
 				response = `Current server settings:\nMean delay: ${currentServerSettings.schedule.meanDelay} minutes\nVariation: ${currentServerSettings.schedule.variation}`;
+			} else {
+				response += 'Note that this does not affect currently scheduled hauntings, but will apply to future ones.';
 			}
 			interaction.reply({ content: response, ephemeral: true });
 			return;

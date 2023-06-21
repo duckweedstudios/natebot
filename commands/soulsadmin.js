c330cffcc286795a84dae8356bb16b53aa5ba81c
coreybriscoe <coreybriscoe@gmail.com>
2023-06-21 17:33:29 -0400

    feat: Add functionality to soulsAdmin command opts

--- commands/soulsadmin.js
+++ commands/soulsadmin.js
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { isGuildSetup } = require('../functions/isSetup.js');
+const { setServerSetting } = require('../functions/set.js');
+const { isMemberDev } = require('../functions/privileges.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,13 +12,43 @@ module.exports = {
 		.addUserOption(userOption => userOption
 			.setName('mean-delay').setDescription('Set the mean delay between haunts in minutes (default 1440 = 1 day)').setRequired(false))
 		.addUserOption(userOption => userOption
-			.setName('variation').setDescription('Set the variation of the delay between haunts from 1 to 10').setRequired(false)),
+			.setName('variation').setDescription('Set the variation of the delay between haunts from 1 to 10 (default 6)').setRequired(false)),
 	async execute(interaction) {
 		if (!await isGuildSetup(interaction)) {
 			interaction.reply({ content: 'This bot has not been setup yet.\n\nTell an admin to use /guildjoin first!', ephemeral: true });
+			return;
 		} else {
-			// await interaction.reply({ content : '🚧 this will be the admin embed 🚧', ephemeral : true });
-            
+			// Check for admin status (for now only developers have access)
+			if (!isMemberDev(interaction.member.id)) {
+				interaction.reply({ content: 'You must be an admin to use this command!', ephemeral: true });
+				return;
+			}
+
+			let response = '';
+			if (interaction.options.getBoolean('reset-defaults')) {
+				setServerSetting(interaction.guild.id, 'meanDelay', 1440);
+				setServerSetting(interaction.guild.id, 'variation', 6);
+				response = 'Server settings reset to defaults.';
+			} else {
+				if (interaction.options.getInteger('mean-delay')) {
+					if (interaction.options.getInteger('mean-delay') < 2) {
+						response += 'Mean delay must be at least 2 minutes.\n';
+					} else {
+						response += `Mean delay updated to ${interaction.options.getInteger('mean-delay')} minutes.\n}`;
+						setServerSetting(interaction.guild.id, 'meanDelay', interaction.options.getInteger('mean-delay'));
+					}
+				}
+				if (interaction.options.getInteger('variation')) {
+					if (interaction.options.getInteger('variation') < 1 || interaction.options.getInteger('variation') > 10) {
+						response += 'Variation must be between 1 and 10.\n';
+					} else {
+						response += `Variation updated to ${interaction.options.getInteger('variation')}.\n}`;
+						setServerSetting(interaction.guild.id, 'variation', interaction.options.getInteger('variation'));
+					}
+				}
+			}
+			interaction.reply({ content: response, ephemeral: true });
+			return;
 		}
 	},
 };

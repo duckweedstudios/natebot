8ab3658003da732be6ea996be0000b3d6b5fb7b8
coreybriscoe <coreybriscoe@gmail.com>
2023-06-21 17:01:17 -0400

    feat: Add options for soulsAdmin slash command

--- commands/soulsadmin.js
+++ commands/soulsadmin.js
@@ -4,7 +4,13 @@ const { isGuildSetup } = require('../functions/isSetup.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('soulsadmin')
-		.setDescription('server admin'),
+		.setDescription('server admin')
+		.addUserOption(userOption => userOption
+			.setName('reset-defaults').setDescription('Reset the server to default settings').setRequired(false))
+		.addUserOption(userOption => userOption
+			.setName('mean-delay').setDescription('Set the mean delay between haunts in minutes (default 1440 = 1 day)').setRequired(false))
+		.addUserOption(userOption => userOption
+			.setName('variation').setDescription('Set the variation of the delay between haunts from 1 to 10').setRequired(false)),
 	async execute(interaction) {
 		if (!await isGuildSetup(interaction)) {
 			interaction.reply({ content: 'This bot has not been setup yet.\n\nTell an admin to use /guildjoin first!', ephemeral: true });

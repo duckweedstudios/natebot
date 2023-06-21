ec3dc7d92ae52395ec97f5a2086b096a7bf6b49b
Zade Dohan <105446965+zade0123@users.noreply.github.com>
2023-06-21 16:20:30 -0400

    Updated deleteInteraction to take token

--- events/deleteInteraction.js
+++ events/deleteInteraction.js
@@ -3,10 +3,10 @@ const axios = require('axios').default;
 
 module.exports = {
 	name: 'deleteInteraction',
-	deleteInteraction : async (interaction) => {
+	deleteInteraction : async (token) => {
 		try {
 			// Retrieves Token
-			const interactionToken = interaction.token;
+			const interactionToken = token;
 			// Patches Message with Data
 			return await axios
 				.delete(`https://discord.com/api/v8/webhooks/${clientId}/${interactionToken}/messages/@original`);

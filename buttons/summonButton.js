747ed2568cb33ee3d7f4f8c2c38e5d5237c5459e
Zade Dohan <105446965+zade0123@users.noreply.github.com>
2023-06-30 19:34:54 -0400

    Summon command now creates and edits prompt

--- buttons/summonButton.js
+++ buttons/summonButton.js
@@ -1,6 +1,8 @@
 const { MessageButton } = require('discord.js');
 const { attemptSummoning } = require('../actions/summoning.js');
 const { SummonAttemptResults } = require('../actions/summoning.js');
+const { deletePrompt } = require('../events/deletePrompt');
+const { editPrompt } = require('../events/editPrompt');
 
 module.exports = {
 	name: 'summonButton',
@@ -12,7 +14,18 @@ module.exports = {
 	async execute(interaction) {
 		const { summonSuccess, cooldown } = await attemptSummoning(interaction.client, interaction.member.guild);
 		if (summonSuccess === SummonAttemptResults.Success) {
-			await interaction.reply({ content:`A soul was summoned! You can summon again in ${cooldown}.`, ephemeral: true });
+			try {
+				deletePrompt(interaction.client.usersCurrentPrompt[interaction.user.id]);
+			} catch (error) {
+				console.log(' ');
+			}
+			interaction.reply({ content: `A soul was summoned! Let's see if someone takes the bait...`, ephemeral: true });
+			interaction.client.usersCurrentPrompt = { ...interaction.client.usersCurrentPrompt, [interaction.user.id] : interaction.token };
+			// Creating Content for Prompt Editing
+
+			const fooledData = { content: `You fooled x fetchers!`, components : [] };
+			setTimeout(() => editPrompt(interaction, fooledData), 23000);
+			setTimeout(() => deletePrompt(interaction.token), 300000);
 		} else if (summonSuccess === SummonAttemptResults.Cooldown) {
 			await interaction.reply({ content:`You cannot summon a soul for ${cooldown}.`, ephemeral: true });
 		} else if (summonSuccess === SummonAttemptResults.TooSoonAfterRealHaunting) {
